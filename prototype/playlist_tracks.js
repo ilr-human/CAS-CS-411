5c6b1c5387a70e61d3064c15209e7a34bcdc4815
Gabriella <33003137+gabriellasroman@users.noreply.github.com>
2020-12-14 09:47:41 -0500

    Fix 'unexpected token' & module errors

--- prototype/playlist_tracks.js
+++ prototype/playlist_tracks.js
@@ -4,11 +4,16 @@ var request = require("request");
 var playlistUrl = "https://api.spotify.com/v1/me/playlists";
 //var token = {};
 
-request({url:topArtistsUrl, headers:{"Authorization":token}}, function(err, res)){
+const spotifyConfig = require('./configs/spotifyAPI')
+const apiKey = spotifyConfig.key
+
+
+request({url:playlistUrl, headers:{"Authorization":apiKey}}, function(err, res){
     if (res){
         var playlist = JSON.parse(res.body);
+        console.log(playlist);
         var playlistUrl = playlist.items[0].href
-        request({url:playlistUrl, headers:{"Authorization":token}}, function(err, res)){
+        request({url:playlistUrl, headers:{"Authorization":apiKey}}, function(err, res){
             if (res){
                 var playlist = JSON.parse(res.body);
                 playlist.tracks.foreach(function(track){
@@ -16,5 +21,5 @@ request({url:topArtistsUrl, headers:{"Authorization":token}}, function(err, res)
                 })
             }
         }       
-    }
-}
+    )}
+})
