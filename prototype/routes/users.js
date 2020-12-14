c61625f7f7d4bad6772ccf218233bce2829c92ca
Quan Viet Pham <35153133+vietquanpham@users.noreply.github.com>
2020-12-15 01:46:55 +0700

    can now create new user on db

--- prototype/routes/users.js
+++ prototype/routes/users.js
@@ -22,6 +22,21 @@ newUser.save()
 .catch(err => res.status(400).json("Error :" + err));
  */
 
+router.post('/add', function(req,res,next){
+    const userID = req.body.userID;
+    const username = req.body.username;
+    const url = req.body.url;
+    const title = req.body.title;
+    const newUser = new User({
+        userID: userID,
+        username: username,
+        gifs: [{url: url, title: title}]
+    });
+    newUser.save()
+        .then(() => res.json("Success"))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
 // get a certain user from db
 router.get('/:id', function(req, res, next){
   User.findOne({userID:req.params.id})
@@ -29,16 +44,22 @@ router.get('/:id', function(req, res, next){
       .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// update an user
+// add a gif to an user's saved gifs
 router.post('/:id', function(req, res, next){
   User.findOne({userID:req.params.id})
       .then(user => {
         // update fields here (using push to add to array)
+        /*
+        user.gifs.push({ url: , title: })
+         */
         user.save()
             .then(() => res.json("Updated!"))
             .catch(err => res.status(400).json('Error: ' + err));
       })
       .catch(err => res.status(400).json('Error: ' + err));
 });
+
+// remove a gif from an user's saved gifs
+// router.delete
 
 module.exports = router;
