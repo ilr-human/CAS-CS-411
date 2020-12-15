c6a0bf4d3a43be9dc4991b0a738a4a81bfba6ab1
yutongs1202 <72466897+yutongs1202@users.noreply.github.com>
2020-12-14 23:53:54 -0500

    Add oauth in app.js

--- prototype/app.js
+++ prototype/app.js
@@ -4,10 +4,14 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-const passport = require('passport')
+const passport = require('passport');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+const spotifyConfig = require('./configs/spotifyAPI');
+const User = require('./models/userModel');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const passportRouter = require('./routes/passport');
 
 const app = express();
 
@@ -32,12 +36,63 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(passport.initialize()); // getting weird errors on this
-// app.use(passport.session());
+app.use(passport.initialize()); // getting weird errors on this
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('passport', passportRouter);
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+
+passport.use(
+    new SpotifyStrategy(
+        {
+          clientID: spotifyConfig.clientID,
+          clientSecret: spotifyConfig.clientSecret,
+          callbackURL: 'http://localhost:3000/auth/spotify/callback'
+        },
+        function(accessToken, refreshToken, expires_in, profile, done) {
+          User.findOne({userID: profile.id})
+              .then(user => console.log(user))
+              .catch(err => {
+                    const userID = profile.id;
+                    const username = profile.name;
+                    const gifs = [];
+                    const newUser = new User({
+                      userID,
+                      username,
+                      gifs
+                    });
+                    newUser.save()
+                    //                      .catch(err => res.status(400).json("Error :" + err));
+                    return done(null, profile);
+                  }
+              )
+          return done(null, profile);
+        }
+    )
+);
+
+app.get('./auth', passport.authenticate('spotify'), function(req, res) {
+    // The request will be redirected to spotify for authentication, so this
+    // function will not be called.
+});
+//
+app.get(
+    '/auth/spotify/callback',
+    passport.authenticate('spotify', { failureRedirect: '/login' }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    }
+);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
